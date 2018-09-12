24f6685ba28c40b28065a2c5fd9ebc317fa72e58
asotafsiri <aso.tafsiri@gmail.com>
2018-09-12 10:06:11 +0430

    handeling the log in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@
 
 # post contoller class
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @posts = Post.all.order('created_at DESC')
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 # route config
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts do
     resources :comments
   end
